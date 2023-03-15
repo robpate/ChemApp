7ca6f7d859ff3f08810784504975cf58280f7ae6
Robert Paterson <robpate@chbc.school.nz>
2023-03-15 14:42:24 +1300

    testing sleep

--- ChemicalEfficiency/Program.cs
+++ ChemicalEfficiency/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ChemicalEfficiency
 {
     internal class Program
     {
+        // Global Variables
+
+        // Lists
+
+        static void ChemTest()
+        {
+
+        }
         static void Main(string[] args)
         {
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"5 seconds should have passed");
+            Console.ReadLine();
         }
     }
 }
